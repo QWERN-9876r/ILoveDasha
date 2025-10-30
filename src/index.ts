9391c3eb9cb4f60ec3faefdbf696892952d924ab
Qwern9876r <dimadobriy111@gmail.com>
2025-10-30 10:59:04 +0300

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import './components/counter-component'
 
-export { CounterComponent } from './components/counter-component'
-export { counterStore } from './stores/counter.store'
-export type { CounterStore } from './stores/counter.store'
