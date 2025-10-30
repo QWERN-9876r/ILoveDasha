ab291bb60f5ccc1adef0eb783a8bddfa4c615584
Qwern9876r <dimadobriy111@gmail.com>
2025-10-30 12:55:51 +0300

    bug fix

--- src/pages/quiz-page.ts
+++ src/pages/quiz-page.ts
@@ -1,4 +1,4 @@
-import { LitElement, PropertyValues, css, html } from 'lit'
+import { LitElement, css, html } from 'lit'
 import { customElement, state } from 'lit/decorators.js'
 
 import '../components/quiz'
